ce9e6d024d284be1def26efe347aa89c8a49732c
Nguyen Tri Anh-PMD <trianhnguyen@vietnamairlines.com>
2018-08-03 17:06:33 +0700

    update

--- Temp/js/Script.js
+++ Temp/js/Script.js
@@ -1,37 +1,40 @@
-// x = window.prompt("Nhập x: ");
-// y = window.prompt("Nhap y: ");
-function F1() {
-    console.log(this);
-    var oldmember = ["a", "b", "c", "d", "e", "f", "g"];
-    var b = document.getElementById("username").value;
-    var memcheck = false;
-    var message = "<b>Hello " + b + "</b>";
-    if (b == "") {
-        console.log("blank check");
-        document.getElementById("content").innerHTML = "Please input Username";
-    } else {
-        for (var i = 0; i < oldmember.length; i++) {
-            if (b == oldmember[i]) {
-                memcheck = true;
-                break;
+document.addEventListener("DOMContentLoaded", function(event) {
+    function F1(event) {
+        document.getElementById("Title1").textContent = "Sap xep so";
+        var x = [];
+        var n= window.prompt("Nhap n");
+        for (var i = 0; i<n ; i++) {
+           var y = window.prompt("Nhập gia tri thu "+i+": "); 
+           x.push(y);
+        }
+        var before=x;
+        document.getElementById("content1").innerHTML +=
+            "<br>Before: "+ before;
+        //Sap xep
+        for (var i = 1; i+1<=x.length; i++) {
+            var temp = x[i];
+            var j=i-1;
+            while (j>=0 && temp > x[j]) {
+                x[j+1] = x[j];
+                x[j] = temp;
+                j--;
             }
         }
-        if (memcheck == true) {
-            message += "</br>This username has been used, Please try again";
-        } else {
-            message += "</br>Welcome to our website";
-        }
-        document.getElementById("content").innerHTML = message;
-    }
+        document.getElementById("content1").innerHTML +=
+            "<br>"+ "After: " + x        ;
+    } // end funtion 1
 
-} // end funtion 1
+    function F2(event) {
+        console.log("F2")
+    } // end funtion 2s
 
-function F2() {
-console.log("F2")
-} // end funtion 2s
+    document.querySelector("#button1")
+        .addEventListener("click", F1);
+    document.querySelector("#button2")
+        .addEventListener("click", F2);
 
-document.querySelector("#button")
-    .addEventListener("click", F1);
-document.querySelector("button")
-.addEventListener("click", F2);
 
+});
+
+// x = window.prompt("Nhập x: ");
+// y = window.prompt("Nhap y: ");
