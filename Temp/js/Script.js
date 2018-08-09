177baccf73ccb9a3e3913d8ca8de5217a5cf5df6
Nguyen Tri Anh-PMD <trianhnguyen@vietnamairlines.com>
2018-08-09 17:30:52 +0700

    update

--- Temp/js/Script.js
+++ Temp/js/Script.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     function F1(event) {
-        document.getElementById("Title1").textContent = "Sap xep so";
-        var x = [];
-        var n= window.prompt("Nhap n");
-        for (var i = 0; i<n ; i++) {
-           var y = window.prompt("Nhập gia tri thu "+i+": "); 
-           x.push(y);
-        }
-        var before=x;
-        document.getElementById("content1").innerHTML +=
-            "<br>Before: "+ before;
-        //Sap xep
-        for (var i = 1; i+1<=x.length; i++) {
-            var temp = x[i];
-            var j=i-1;
-            while (j>=0 && temp > x[j]) {
-                x[j+1] = x[j];
-                x[j] = temp;
-                j--;
+        document.getElementById("Title1").textContent = "In so";
+        var x = [2];
+        var n = window.prompt("Nhap n");
+        // for (var i = 0; i < n; i++) {
+        //     var y = window.prompt("Nhập gia tri thu " + i + ": ");
+        //     x.push(y);
+        // }
+        // document.getElementById("content1").innerHTML +=
+        //     "<br>Before: " + x;
+        // Lap danh sach so nguyen to
+        for (var i = 3; i < n; i++) {
+            var check = true;
+            for (var j = 0; j + 1 <= x.length; j++) {
+                if (i % x[j] == 0) { check = false; }
             }
+            if (check) { x.push(i) };
+        }
+        var c2 = true;
+        while (n) {
+            
+            if (n % i == 0) {}
         }
         document.getElementById("content1").innerHTML +=
-            "<br>"+ "After: " + x        ;
+            x;
     } // end funtion 1
 
     function F2(event) {
